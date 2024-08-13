b3f2e958dba43343d1c69339c29a5e522c275b73
Facundo Marroquin <A01383541@tec.mx>
2024-08-13 10:53:21 -0600

    Facundo

--- lession1/HelloWorld.java
+++ lession1/HelloWorld.java
@@ -2,6 +2,6 @@ package Javabootcamp.lession1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("¡Hola, mundo!");
+        System.out.println("¡Hola, Facundo!");
     }
 }
